cfe4318b3ac1a55ec6a6a8d178f127854a7d43dc
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-05-14 08:48:19 -0600

    Initial Model Creation

--- credentialstore/api/urls.py
+++ credentialstore/api/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ClientListView, ClientCreateView
 
+
 urlpatterns = {
-    url(r'^clientlist$', ClientListView.as_view(), name="list"),
-    url(r'^clientcreate$', ClientCreateView.as_view(), name="create"),
+    url(r'^ListClient$', ClientListView.as_view(), name="list"),
+    url(r'^CreateClient$', ClientCreateView.as_view(), name="create"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
