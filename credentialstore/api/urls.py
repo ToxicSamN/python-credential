65383dcb8a80bd693b0bfce852de6f383a20432a
Sammy <sammy.d.shuck@gmail.com>
2018-05-15 06:38:09 -0600

    Modified api/urls.py for some basic tests.

--- credentialstore/api/urls.py
+++ credentialstore/api/urls.py
@@ -2,11 +2,13 @@
 
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ClientListView, ClientCreateView, SecretListView
+from .views import ClientListView, ClientCreateView, SecretListView, RealClientListView, ClientDetailsView
 
 
 urlpatterns = {
-    url(r'^ListClient$', ClientListView.as_view(), name="list"),
+    url(r'^GetClient$', RealClientListView.as_view(), name="list"),
+    # url(r'^client/(?P<ClientId>[0-9a-z]{1,65535})/$', ClientDetailsView.as_view(), name="details"),
+    # url(r'^RealListClient$', RealClientListView.as_view(), name="list"),
     url(r'^CreateClient$', ClientCreateView.as_view(), name="create"),
     url(r'^GetSecret$', SecretListView.as_view(), name="get_secret"),
 }
