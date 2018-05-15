fd391102998a47762cfb9ce5f14a66b1dd5c1db2
Sammy <sammy.d.shuck@gmail.com>
2018-05-14 22:36:41 -0600

    Updated views.py to retrieve the pubkey of a client.

--- credentialstore/api/views.py
+++ credentialstore/api/views.py
@@ -1,17 +1,32 @@
 # api/views.py
 
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import ClientListSerializer, ClientCreateSerializer, SecretListSerializer, SecretCreateSerializer
 from .models import ClientModel, SecretModel
 from .pystuffing.secret import Secret
 from .pystuffing.client import Client
 
 
-class ClientListView(generics.ListAPIView):
+class ClientListView(generics.CreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = ClientModel.objects.all()
     serializer_class = ClientListSerializer
+
+    def create(self, request, *args, **kwargs):
+        client_id = None
+
+        if request.data.get('string_data'):
+            client_id = Client.create_clientid(request.data['string_data'])
+        elif request.data.get('ClientId'):
+            client_id = request.data['ClientId']
+
+        client = Client(ClientModel, ClientId=client_id)
+        if not client.validate():
+            return Response(['HTTP 400 BAD REQUEST. Server does not exist', request.data])
+
+        return Response(['HTTP 200 OK', {'PublicKey': client.data.pubkey, 'ClientName': client.data.name}], status=status.HTTP_200_OK)
 
 
 class ClientCreateView(generics.CreateAPIView):
