2f3fab3676a94bbb345559e67f29b7fb6541ab5d
Sammy <sammy.d.shuck@gmail.com>
2018-05-14 22:37:08 -0600

    Updated client.py

--- credentialstore/api/pystuffing/client.py
+++ credentialstore/api/pystuffing/client.py
@@ -1,10 +1,27 @@
 import os
 import sys
 import platform
+import hashlib
 from pycrypt.encryption import Encryption
 
 
 class Client(Encryption):
-    pass
 
-print('break')
+    def __init__(self, model, ClientId):
+        self.model = model
+        self.client_id = ClientId
+        self.data = None
+        super().__init__()
+
+    @staticmethod
+    def create_clientid(string_data):
+        hsh = hashlib.sha512(string_data.encode())
+        return hsh.hexdigest()
+
+    def validate(self):
+        self.data = self.model.objects.filter(ClientId=self.client_id)
+        if not self.data:
+            return None
+        else:
+            self.data = self.data[0]
+            return True
