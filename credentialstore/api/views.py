4a2a472ce4ba05bf044a0daf7d02e2688cdfed84
Sammy <sammy.d.shuck@gmail.com>
2018-05-15 06:38:47 -0600

    Modified views.py for testing

--- credentialstore/api/views.py
+++ credentialstore/api/views.py
@@ -1,12 +1,48 @@
 # api/views.py
 
+import json
+from functools import reduce
 from django.shortcuts import render
+from django.core import serializers, exceptions
+from django.core.serializers.json import DjangoJSONEncoder
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import ClientListSerializer, ClientCreateSerializer, SecretListSerializer, SecretCreateSerializer
 from .models import ClientModel, SecretModel
 from .pystuffing.secret import Secret
 from .pystuffing.client import Client
+
+
+class RealClientListView(generics.ListAPIView):
+    queryset = ClientModel.objects.all()
+    serializer_class = ClientListSerializer
+
+    def get(self, request, *args, **kwargs):
+
+        if request.query_params:
+            try:
+                queryset = ClientModel.objects.filter(**request.query_params.dict())
+            except exceptions.FieldError as e:
+                replace_str = ('name', ''), ('date_created', ''), ('date_modified', ''), ('id', ''), (' ,', '')
+                msg = e.args[0]
+                return Response(
+                    ["HTTP 400 BAD REQUEST", "{}".format(reduce(lambda a, kv: a.replace(*kv), replace_str, msg))],
+                    status=status.HTTP_400_BAD_REQUEST)
+                # raise exceptions.FieldError("{}".format(reduce(lambda a, kv: a.replace(*kv), replace_str, msg)),
+                #                             status=status.HTTP_400_BAD_REQUEST)
+
+            if queryset:
+                serializer_q = ClientListSerializer(queryset, many=True)
+                return Response(serializer_q.data, status=status.HTTP_200_OK)
+
+        return Response(["HTTP 400 BAD REQUEST", "Must provide a valid query"], status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ClientDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClientModel.objects.all()
+    serializer_class = ClientListSerializer
+    lookup_field = 'ClientId'
 
 
 class ClientListView(generics.CreateAPIView):
@@ -26,7 +62,12 @@ class ClientListView(generics.CreateAPIView):
         if not client.validate():
             return Response(['HTTP 400 BAD REQUEST. Server does not exist', request.data])
 
-        return Response(['HTTP 200 OK', {'PublicKey': client.data.pubkey, 'ClientName': client.data.name}], status=status.HTTP_200_OK)
+        return Response(['HTTP 200 OK',
+                         {'PublicKey': client.data.pubkey,
+                          'ClientName': client.data.name
+                         }
+                        ],
+                        status=status.HTTP_200_OK)
 
 
 class ClientCreateView(generics.CreateAPIView):
