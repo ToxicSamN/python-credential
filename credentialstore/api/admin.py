33cae458e44a46d631c153f811528778124726f0
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-05-24 13:48:59 -0600

    added new Admin model

--- credentialstore/api/admin.py
+++ credentialstore/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SecretModel, ClientModel
+from .models import SecretModel, ClientModel, AdminModel
 
 # Register your models here.
 admin.site.register(SecretModel)
 admin.site.register(ClientModel)
+admin.site.register(AdminModel)
