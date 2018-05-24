4717d4e3e74a48b387b858850c2f461a1aea633d
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-05-24 13:47:30 -0600

    Adding Admin access urls

--- credentialstore/api/urls.py
+++ credentialstore/api/urls.py
@@ -2,15 +2,22 @@
 
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ClientListView, ClientCreateView, SecretListView, RealClientListView, ClientDetailsView
+from .views import GetCredentialView, CreateClientView, UpdateClientView, AdminGetClientsView, AdminGetClientView
+from .views import AdminGetSecretsView, AdminGetSecretView, AdminGetSecretClientsView
+from .views import AdminDeleteClient, AdminDeleteSecret
 
 
 urlpatterns = {
-    url(r'^GetClient$', RealClientListView.as_view(), name="list"),
-    # url(r'^client/(?P<ClientId>[0-9a-z]{1,65535})/$', ClientDetailsView.as_view(), name="details"),
-    # url(r'^RealListClient$', RealClientListView.as_view(), name="list"),
-    url(r'^CreateClient$', ClientCreateView.as_view(), name="create"),
-    url(r'^GetSecret$', SecretListView.as_view(), name="get_secret"),
+    url(r'^GetCredential$', GetCredentialView.as_view(), name="get_cred"),
+    url(r'^CreateClient$', CreateClientView.as_view(), name="create_client"),
+    url(r'^UpdateClient$', UpdateClientView.as_view(), name="create_client"),
+    url(r'^admin/GetAllClients$', AdminGetClientsView.as_view(), name="adm_all_clients"),
+    url(r'^admin/GetAllSecrets$', AdminGetSecretsView.as_view(), name="adm_all_secrets"),
+    url(r'^admin/GetClient$', AdminGetClientView.as_view(), name="adm_client"),
+    url(r'^admin/GetSecret$', AdminGetSecretView.as_view(), name="adm_secret"),
+    url(r'^admin/GetSecretClients$', AdminGetSecretClientsView.as_view(), name="adm_secret_client"),
+    url(r'^admin/DeleteClient$', AdminDeleteClient.as_view(), name="adm_del_client"),
+    url(r'^admin/DeleteSecret$', AdminDeleteSecret.as_view(), name="adm_del_secret"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
