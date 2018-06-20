9e40231743c36c8208e2ff18b1c0b9b4967a0a0d
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-06-20 14:24:41 -0700

    debug param

--- credentialstore/credentialstore/settings.py
+++ credentialstore/credentialstore/settings.py
@@ -45,7 +45,7 @@ LOGGING = {
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print (os.environ)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
