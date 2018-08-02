a0b7101dc8559ee782f062761f73cdd542b7a41a
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-02 09:26:44 -0600

    admin model creation

--- credentialstore/api/admin.py
+++ credentialstore/api/admin.py
@@ -4,7 +4,16 @@ from django.contrib import admin
 #from credentialstore.credentialstore.admin import CustomAdminSite
 from .models import SecretModel, ClientModel, AdminModel
 
+
+class SecretAdminModel(admin.ModelAdmin):
+    list_display = ('username')
+
+
+class ClientAdminModel(admin.ModelAdmin):
+    list_display = ('ClientId', 'name')
+
+
 # Register your models here.
-admin.site.register(SecretModel)
-admin.site.register(ClientModel)
+admin.site.register(SecretModel, SecretAdminModel)
+admin.site.register(ClientModel, ClientAdminModel)
 admin.site.register(AdminModel)
