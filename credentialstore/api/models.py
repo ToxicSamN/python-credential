1cb594df4a68e0c07884b82a97bf7b02ba38def6
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-02 09:51:44 -0600

    model grooming

--- credentialstore/api/models.py
+++ credentialstore/api/models.py
@@ -48,10 +48,3 @@ class ClientModel(models.Model):
         Return the Human Readable representation of the model
         """
         return "ID: {}\nPublicKey: {}".format(self.id, self.pubkey)
-
-
-class AdminModel(models.Model):
-
-    AdminId = models.CharField(max_length=256, blank=False, unique=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
