d3e161568e4c61a63e5773f7e87d0ec34b79b9ab
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-02 18:52:18 -0600

    model changes

--- credentialstore/api/forms.py
+++ credentialstore/api/forms.py
@@ -24,17 +24,3 @@ class SecretWithCheckBoxForm(SecretModelForm):
         fields = SecretModelForm.Meta.fields
         fields.append('show_password',)
 
-
-class PasswordField(forms.CharField):
-    widget = forms.PasswordInput
-
-
-class PasswordModelField(models.CharField):
-    """
-    This class is to create a new model form field type for passwords
-    This isn't built in to the model forms by default
-    """
-    def formfield(self, **kwargs):
-        defaults = {'form_class': PasswordField}
-        defaults.update(kwargs)
-        return super(PasswordModelField, self).formfield(**defaults)
--- credentialstore/api/widgets.py
+++ credentialstore/api/widgets.py
@@ -1,2 +1,19 @@
 #  api/widgets.py
 
+from django import forms
+from django.db import models
+
+
+class PasswordField(forms.CharField):
+    widget = forms.PasswordInput
+
+
+class PasswordModelField(models.CharField):
+    """
+    This class is to create a new model form field type for passwords
+    This isn't built in to the model forms by default
+    """
+    def formfield(self, **kwargs):
+        defaults = {'form_class': PasswordField}
+        defaults.update(kwargs)
+        return super(PasswordModelField, self).formfield(**defaults)
