bdc2f5e63b2a8b4596287e69764844ced7d9f8dc
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-03 13:20:21 -0600

    modelform changes

--- credentialstore/api/admin.py
+++ credentialstore/api/admin.py
@@ -2,13 +2,41 @@
 
 from django.contrib import admin
 from .models import SecretModel, ClientModel
-from .forms import SecretModelForm, SecretWithCheckBoxForm
+from .forms import SecretModelForm
+from .pystuffing.secret import Secret
 
 
 class SecretAdminModel(admin.ModelAdmin):
     form = SecretModelForm
+    change_form_template = 'admin/change_secret_form.html'
+    add_form_template = 'admin/change_secret_form.html'
     list_display = ('username',)
 
+    def save_model(self, request, obj, form, change):
+        """
+        Method override so that the password that is supplied by the user is encrypted before saving to db
+        :param request:
+        :param obj:
+        :param form:
+        :param change:
+        :return:
+        """
+        if obj.pk:
+            model_obj = self.model.objects.get(pk=obj.pk)
+            if not model_obj.password == obj.password:
+                # password changed and we need to now encrypt the password
+                obj = self.decoder_ring(obj)
+        else:
+            obj = self.decoder_ring(obj)
+
+        obj.save()
+
+    @staticmethod
+    def decoder_ring(obj):
+        decoder_ring = Secret()
+        decoder_ring.encrypt(privateData=obj.password)
+        obj.password = decoder_ring.get_encrypted_message().decode('utf-8')
+        return obj
 
 class ClientAdminModel(admin.ModelAdmin):
     list_display = ('ClientId', 'name',)
