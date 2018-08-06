b4f181ef3b4c2a54aef7fa59d9ffc5d66477e82a
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-06 16:51:45 -0600

    id button

--- credentialstore/assets/js/generate_clientid.js
+++ credentialstore/assets/js/generate_clientid.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
    $('#generate_id').click(function () {
         $.get('https://y0319t11888/credentialstore/NewClientId', function (data, status) {
             $('#id_ClientId').prop('value', data);
-        });
+        }, json);
     });
 });
