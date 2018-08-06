11116ffad9dd7dee4a2aa9310dc0b79275f3d97d
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-06 16:25:14 -0600

    generate button

---
+++ credentialstore/assets/js/generate_clientid.js
@@ -0,0 +1,14 @@
+
+function generateClientId()
+{
+    const api = 'https://localhost/credentialstore/NewClientId';
+    const clientId = document.getElementById('id_ClientId');
+    var request = XMLHttpRequest();
+    request.open('GET', api, true);
+    request.onload = function() {
+        var data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            clientId.value = data.ClientId
+        }
+    }
+}
