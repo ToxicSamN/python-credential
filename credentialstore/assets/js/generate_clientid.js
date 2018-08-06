1021553b749fcddc0e10c9429974ba1ac1ab8149
Sammy <31512872+ToxicSamN@users.noreply.github.com>
2018-08-06 16:41:50 -0600

    id button

--- credentialstore/assets/js/generate_clientid.js
+++ credentialstore/assets/js/generate_clientid.js
@@ -1,17 +1,7 @@
 
-function generateClientId()
-{
-    const api = 'https://localhost/credentialstore/NewClientId';
-    const clientId = document.getElementById('id_ClientId');
-    var request = new XMLHttpRequest();
-    request.open('GET', api, true);
-    request.onload = parse_data(request);
-    request.send();
-}
 
-function parse_data(request) {
-    var data = JSON.parse(request.response);
-    if (request.status >= 200 && request.status < 400) {
-        clientId.value = data.ClientId;
-    }
-}
+$('generate_id').click(function(){
+   $.get('http://y0319t11888/credentialstore/NewClientId', function(data, status){
+      $('#id_ClientId').prop('value', data);
+   });
+});
